e01dee28ad9a348dedd84093b119a9795a323379
Xi0n3 <108387398+Xi0n3@users.noreply.github.com>
2025-02-10 02:52:29 -0600

    First Commit

---
+++ game/test/thirdControllerPersonTest/GameModules/Player/player.gd
@@ -0,0 +1,114 @@
+extends CharacterBody3D
+#Señales
+signal sprinting_changed(is_sprinting)
+
+@export_group("Components")
+@export var view: Node3D
+
+@export_group("Properties")
+@export_group("Properties/Movement")
+@export var walking_speed = 1.5
+@export var running_speed = 4.0
+@export var sprint_speed = 7.0
+
+@export var jump_velocity = 4.5
+
+@export_group("Control Properties")
+@export var controller_value_stick_to_start_run = 0.6
+
+#Podemos agregar una linea como la de abajo para instanciar un nodo de la escena
+#en una variable, simplemente arrastrando el nodo al código mientras mantenemos CTRL
+@onready var camera_mount = $camRoot
+@onready var visuals = $visuals
+@onready var animation_player = $AnimationPlayer
+var rotation_direction: float
+
+var SPEED = walking_speed
+var JUMP_VELOCITY = jump_velocity
+var running =  false
+var walking_mode = false
+var is_locked = false
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	Engine.max_fps = 60;
+
+#Funcion para escuchar cualquier evento
+func _process(delta):
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		elif Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+	if Input.is_action_just_pressed("walking_mode"):
+		if walking_mode: walking_mode=false; print("walkmodeOFF")
+		else: walking_mode = true;  print("walkmodeON")
+
+func _physics_process(delta):
+	#Manda una señal a View para el fov de la camara
+	sprinting_changed.emit(false)
+	if InputEventJoypadMotion:  # Control
+		walking_mode=false
+		
+	if animation_player.current_animation == "kick":
+		var progress = animation_player.current_animation_position / animation_player.current_animation_length
+		if progress >= 0.5:
+			is_locked = false  # Desbloquea el movimiento cuando la animación llega al 30%
+			 
+	if !animation_player.is_playing() and animation_player.current_animation != "kick":
+		is_locked = false  # Desbloquea completamente cuando la animación termina
+		
+	if Input.is_action_just_pressed("attack"):
+		if animation_player.current_animation != "kick":
+			animation_player.play("kick", -1, 1.5)
+			is_locked = true
+		
+	else:
+		SPEED = walking_speed
+		running= false
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		if !is_locked:
+			if  (!walking_mode && (abs(input_dir.x) + abs(input_dir.y))>controller_value_stick_to_start_run):
+				if Input.is_action_pressed("sprint"):
+					if animation_player.current_animation != "running":
+						animation_player.play("running", -1, 1.5)
+					SPEED = sprint_speed
+					sprinting_changed.emit(true) 
+				else:
+					if animation_player.current_animation != "running":
+						animation_player.play("running")
+					SPEED = running_speed
+					running = true
+			else:
+				if animation_player.current_animation != "walking":
+					animation_player.play("walking")
+			visuals.look_at(position + direction)
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		if !is_locked:
+			if animation_player.current_animation != "idle":
+				animation_player.play("idle")
+		
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	if !is_locked:
+		move_and_slide()
---
+++ game/test/thirdControllerPersonTest/GameModules/Player/view.gd
@@ -0,0 +1,55 @@
+extends Node3D
+@onready var camera_3d = $SpringArm3D/Camera3D
+@onready var player = $".."
+@onready var visuals = $"../visuals"
+@export_group("Control Settings")
+@export var controller_camera_sens_horizontal = 0.08
+@export var controller_camera_sens_vertical = -0.08
+@export_group("Mouse Settings")
+@export var mouse_camera_sens_horizontal = 0.15
+@export var mouse_camera_sens_vertical = 0.15
+@export_group("Settings")
+@export var default_fov = 75
+@export var sprint_fov = 90
+@export var transition_speed = 5.0
+@export var max_rotation_y = 45
+@export var min_rotation_y = -70
+
+
+var input := Vector3.ZERO
+
+func _ready():
+	player.sprinting_changed.connect(_on_sprinting_changed)
+
+func _physics_process(delta):
+	control_input()
+	
+	player.rotate_y(input.x) #voltea al player y con la camara en el eje Y
+	visuals.rotate_y(-input.x) #Evita que el modelo del player se gira con la camara (así la direccion siempre coincide donde mire la camara)
+	#
+	#print("deg: "+ str(rad_to_deg(rotation.x)))
+	#print("radianes: " + str(deg_to_rad((rotation.x))))
+	#La camara se puede mover en el eje de rotacion x si...
+	#recordar pasar rotationa de radianes a grados para que sea más entendible
+	if ((rad_to_deg(rotation.x) < max_rotation_y || input.y<0) && #puede moverse para abajo
+	(rad_to_deg(rotation.x) > min_rotation_y || input.y>0)): #puede moverse para arriba
+		rotate_x(input.y)
+		
+	input.x=0; input.y=0
+	
+
+func control_input():
+	var cam_input_dir = Input.get_vector("camera_right", "camera_left", "camera_up", "camera_down")
+	if cam_input_dir:
+		input.x = cam_input_dir.x*controller_camera_sens_horizontal
+		input.y = cam_input_dir.y*controller_camera_sens_vertical
+	
+func _input(event):
+	#mouse input values
+	if event is InputEventMouseMotion && Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		input.x = -deg_to_rad(event.relative.x*mouse_camera_sens_horizontal)
+		input.y = -deg_to_rad(event.relative.y*mouse_camera_sens_vertical)
+		
+func _on_sprinting_changed(is_sprinting):
+	var target_fov = sprint_fov if is_sprinting else default_fov
+	create_tween().tween_property(camera_3d, "fov", target_fov, 0.3).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
