0e522e3e97be6d11253f7ab5eccc59121922a057
iNax <rgalarza2003@hotmail.com>
2025-02-22 23:56:48 -0600

    Version 1.1 iNax
    El pendejo de moran me hizo rehacer todo el codigo que ya servia
    Gracias por pendejarme

--- game/test/thirdControllerPersonTest/GameModules/Player/movement_controller.gd
+++ game/test/thirdControllerPersonTest/GameModules/Player/movement_controller.gd
@@ -9,55 +9,58 @@ enum STATE {
 	FALLING,
 	ATTACKING,
 }
-
+@onready var visuals = $"../visuals"
 @onready var player = $".."
 @onready var animation_player = $"../AnimationPlayer"
 
+@onready var SPEED = player.walking_speed
 var current_state: STATE = STATE.IDLE
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var is_locked = false
 
 func _physics_process(delta):
-	var direction = Vector3.ZERO
+	#Aqui checamos el currentState
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var direction = (player.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
-	if Input.is_action_pressed("right"):
-		direction.x += 1
-	if Input.is_action_pressed("left"):
-		direction.x -= 1
-	if Input.is_action_pressed("forward"):
-		direction.z -= 1
-	if Input.is_action_pressed("backward"):
-		direction.z += 1
 	
-	if direction.length() > 0:
-		if Input.is_action_pressed("sprint"):
-			current_state = STATE.SPRINTING
-			player.velocity.x += 7.0
-		else:
-			current_state = STATE.RUNNING
-	else:
-		current_state = STATE.IDLE
-	
-	if Input.is_action_just_pressed("ui_accept") and player.is_on_floor():
-		current_state = STATE.JUMPING
-		player.velocity.y = 4.5
-	
-	if not player.is_on_floor():
-		current_state = STATE.FALLING
-	
-	if Input.is_action_just_pressed("attack"):
-		current_state = STATE.ATTACKING	
-
 	# Aplicar animaciones según el estado
 	match current_state:
 		STATE.IDLE:
-			animation_player.play("Movement/idle")
+			Idle()
+			if direction:
+				current_state= STATE.WALKING
+		STATE.WALKING:
+			if ((abs(input_dir.x) + abs(input_dir.y))>player.controller_value_stick_to_start_run): #NO OLVIDAR PONER !WalkingMode
+				current_state = STATE.RUNNING
 		STATE.RUNNING:
-			animation_player.play("Movement/run")
+			
+			if Input.is_action_pressed("sprint"):
+				current_state = STATE.SPRINTING
 		STATE.SPRINTING:
-			animation_player.play("Movement/fast_run")
-		STATE.JUMPING:
-			animation_player.play("Movement/jump")
-		STATE.FALLING:
-			animation_player.play("Movement/fall")
-		STATE.ATTACKING:
-			animation_player.play("Movement/sword_slash_1", -1, 1.4)
+			print()
+		#STATE.JUMPING:
+		#STATE.FALLING:
+		#STATE.ATTACKING:
+	GeneralMovement(direction)
+	print(current_state)
+
+func Idle():
+	current_state = STATE.IDLE
+	animation_player.play("Movement/idle")
+
+func Sprint():
+	animation_player.play("Movement/fast_run")
+	SPEED = player.sprint_speed
+	#sprinting_changed.emit(true)
+
+
+func GeneralMovement(direction):
+	if direction:
+		if not is_locked:
+			visuals.look_at(player.position + direction)
+		player.velocity.x = direction.x * SPEED
+		player.velocity.z = direction.z * SPEED
+	else:
+		player.velocity.x = move_toward(player.velocity.x, 0, SPEED)
+		player.velocity.z = move_toward(player.velocity.z, 0, SPEED)
