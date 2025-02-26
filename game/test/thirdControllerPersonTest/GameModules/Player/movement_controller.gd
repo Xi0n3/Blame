369916f67bc459576f643d37c1d336d64a3d228f
Xi0n3 <108387398+Xi0n3@users.noreply.github.com>
2025-02-26 01:17:29 -0600

    Decartar maquina de estados prototipo

--- game/test/thirdControllerPersonTest/GameModules/Player/movement_controller.gd
+++ game/test/thirdControllerPersonTest/GameModules/Player/movement_controller.gd
@@ -1,66 +1,40 @@
 extends Node
-
-enum STATE {
-	IDLE,
-	WALKING,
-	RUNNING,
-	SPRINTING,
-	JUMPING,
-	FALLING,
-	ATTACKING,
-}
-@onready var visuals = $"../visuals"
-@onready var player = $".."
-@onready var animation_player = $"../AnimationPlayer"
-
-@onready var SPEED = player.walking_speed
-var current_state: STATE = STATE.IDLE
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-var is_locked = false
-
-func _physics_process(delta):
-	#Aqui checamos el currentState
-	var input_dir = Input.get_vector("left", "right", "forward", "backward")
-	var direction = (player.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	
-	
-	# Aplicar animaciones según el estado
-	match current_state:
-		STATE.IDLE:
-			Idle()
-			if direction:
-				current_state= STATE.WALKING
-		STATE.WALKING:
-			if ((abs(input_dir.x) + abs(input_dir.y))>player.controller_value_stick_to_start_run): #NO OLVIDAR PONER !WalkingMode
-				current_state = STATE.RUNNING
-		STATE.RUNNING:
-			
-			if Input.is_action_pressed("sprint"):
-				current_state = STATE.SPRINTING
-		STATE.SPRINTING:
-			print()
-		#STATE.JUMPING:
-		#STATE.FALLING:
-		#STATE.ATTACKING:
-	GeneralMovement(direction)
-	print(current_state)
-
-func Idle():
-	current_state = STATE.IDLE
-	animation_player.play("Movement/idle")
-
-func Sprint():
-	animation_player.play("Movement/fast_run")
-	SPEED = player.sprint_speed
-	#sprinting_changed.emit(true)
-
-
-func GeneralMovement(direction):
-	if direction:
-		if not is_locked:
-			visuals.look_at(player.position + direction)
-		player.velocity.x = direction.x * SPEED
-		player.velocity.z = direction.z * SPEED
-	else:
-		player.velocity.x = move_toward(player.velocity.x, 0, SPEED)
-		player.velocity.z = move_toward(player.velocity.z, 0, SPEED)
+#
+#enum {
+	#IDLE,
+	#WALKING,
+	#RUNING,
+	#SPRINTING,
+	#JUMPING,
+	#FALLING,
+	#ATTACKING,
+#}
+##Nodos
+#@onready var player = $".."
+#@onready var animation_player = $"../AnimationPlayer"
+#
+#var CURRENT_STATE
+#
+## Get the gravity from the project settings to be synced with RigidBody nodes.
+#var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+#
+	##SPEED = player.walking_speed
+	##JUMP_VELOCITY = player.jump_velocity
+#
+#func _physics_process(delta):
+	#print(str(player.walking_speed))
+	#idle(delta)
+	#
+#
+#func idle(delta):
+	#animation_player.play("Movement/idle")
+	#not_on_floor(delta)
+#
+#func walk(delta):
+	#animation_player.play("Movement/walk")
+	#not_on_floor(delta)
+#
+#
+#func not_on_floor(delta):
+	#print("gravity")
+	##player.velocity.y -= gravity * delta
--- game/test/thirdControllerPersonTest/GameModules/Player/player.gd
+++ game/test/thirdControllerPersonTest/GameModules/Player/player.gd
@@ -1,3 +1,6 @@
+#Este código necesita migrar su movimiento a movement controller, 
+#y un Input Controller, ademas de ser más modular, pero todo a su tiempo
+
 extends CharacterBody3D
 #Señales
 signal sprinting_changed(is_sprinting)
@@ -12,15 +15,20 @@ signal sprinting_changed(is_sprinting)
 @export var sprint_speed = 7.0
 
 @export var jump_velocity = 4.5
- 
+ 	
 @export_group("Control Properties")
 @export var controller_value_stick_to_start_run = 0.6
 
+#Podemos agregar una linea como la de abajo para instanciar un nodo de la escena
+#en una variable, simplemente arrastrando el nodo al código mientras mantenemos CTRL
 @onready var visuals = $visuals
 @onready var animation_player = $AnimationPlayer
 var rotation_direction: float
 
 var SPEED = walking_speed
+var JUMP_VELOCITY = jump_velocity
+var running =  false
+var walking_mode = false
 var is_locked = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -30,45 +38,77 @@ func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	Engine.max_fps = 60;
 
+#Funcion para escuchar cualquier evento
 func _process(delta):
+	
 	if Input.is_action_just_pressed("ui_cancel"):
 		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		elif Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
+	if Input.is_action_just_pressed("walking_mode"):
+		if walking_mode: walking_mode=false; print("walkmodeOFF")
+		else: walking_mode = true;  print("walkmodeON")
+
 func _physics_process(delta):
-	# Manda una señal a View para el fov de la camara
+	#Manda una señal a View para el fov de la camara
 	sprinting_changed.emit(false)
-	
+		
 	if animation_player.current_animation == "Movement/sword_slash_1":
 		var progress = animation_player.current_animation_position / animation_player.current_animation_length
 		if progress >= 0.65:
-			is_locked = false
-	
-	if not animation_player.is_playing() and animation_player.current_animation != "Movement/sword_slash_1":
-		is_locked = false
-	
+			is_locked = false  # Desbloquea el movimiento cuando la animación llega al 30%
+			 
+	if !animation_player.is_playing() and animation_player.current_animation != "Movement/sword_slash_1":
+		is_locked = false  # Desbloquea completamente cuando la animación termina
+		
 	if Input.is_action_just_pressed("attack"):
 		if animation_player.current_animation != "Movement/sword_slash_1":
 			animation_player.play("Movement/sword_slash_1", -1, 1.4)
 			is_locked = true
-
-	# Aplicar gravedad
+		
+	else:
+		SPEED = walking_speed
+		running= false
+	
+	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Obtener dirección de entrada sin modificar estados
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		if not is_locked:
+		if !is_locked:
+			if  (!walking_mode && (abs(input_dir.x) + abs(input_dir.y))>controller_value_stick_to_start_run):
+				if Input.is_action_pressed("sprint"):
+					if animation_player.current_animation != "Movement/fast_run":
+						animation_player.play("Movement/fast_run")
+					SPEED = sprint_speed
+					sprinting_changed.emit(true) 
+				else:
+					if animation_player.current_animation != "Movement/run":
+						animation_player.play("Movement/run")
+					SPEED = running_speed
+					running = true
+			else:
+				if animation_player.current_animation != "Movement/walk":
+					animation_player.play("Movement/walk")
 			visuals.look_at(position + direction)
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
+		if !is_locked:
+			if animation_player.current_animation != "Movement/idle":
+				animation_player.play("Movement/idle")
+		
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	if not is_locked:
-		move_and_slide()
+	if !is_locked:
+		move_and_slide() 
